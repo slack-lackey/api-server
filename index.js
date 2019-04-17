21f1e67e296db3d991ef50b7e346a3f60000f347
christopherknightmerritt <chris.merritt95@gmail.com>
2019-04-16 17:32:59 -0700

    fix shampe

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 'use strict';
 require('dotenv').config();
+
 require('./src/app.js').start(process.env.PORT);
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,28 +2,58 @@
 const superagent = require('superagent');
 
 const express = require('express');
-const moment = require('moment');
 const errorHandler = require(`../middleware/500.js`);
+const modelFinder = require(`../middleware/model-finder.js`);
 
 const router = express.Router();
 
-router.get('/getAll', handleGetAll);
+router.use(modelFinder);
+
+// router.get('/getAll', handleGetAll);
+// router.get('/gistCategory', gistCategory);
 router.post('/createGist', createGist);
+// router.post('/test', testPost);
+
+// function testPost(request, response){
+//   request.model.post(request.body);
+//   response.status(200).send('ok');
+// }
+
+// function gistCategory(request, response){
+//   request.model.get()
+//     .then(res => {
+//       let result = [];
+//       res.forEach(item => {
+//         if(item.keywords.includes(request.query.check)){
+//           result.push(item.url);
+//         }
+//       });
+//       return result;
+//     })
+//     .then(result => {
+//       console.log(result);
+//       response.status(200).send(result);
+//     });
+// }
 
 function createGist(request, response){
+  // console.log(request.body);
+  // let username = request.body.username.replace(/\s+/g, '-').toLowerCase()+ '-' + Date.now() + '.js';
+  // let content = request.body.text;
+  // let gistContent = content.slice(content.indexOf('```')+3, content.lastIndexOf('```'));
+  // let date = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 
-  let username = request.body.username.replace(/\s+/g, '-').toLowerCase()+ '-' + Date.now() + '.js';
-  let content = request.body.text;
-  let gistContent = content.slice(content.indexOf('```')+3, content.lastIndexOf('```'));
+  console.log(request.body);
+  let filename = request.body.filename;
 
   return superagent.post('https://api.github.com/gists')
     .set('Authorization', `token ${process.env.GIST_TOKEN}`)
     .send({
-      'description': 'Gist created by ' + request.body.username + ' on ' + moment().format('dddd, MMMM Do YYYY, h:mm:ss a') + ' in Slack.',
+      'description': request.body.description,
       'public': true,
       'files': {
-        [username] : {
-          'content': gistContent,
+        [filename] : {
+          'content': request.body.content,
         },
       },
     })
@@ -36,12 +66,12 @@ function createGist(request, response){
     .catch(errorHandler);
 }
 
-function handleGetAll(request, response, next) {
-  return superagent.get('https://api.github.com/users/SlackLackey/gists')
-    .then(res => {
-      console.log(res.body[0].url);
-      response.status(200).json(res.body[0].url);
-    });
-}
+// function handleGetAll(request, response, next) {
+//   return superagent.get('https://api.github.com/users/SlackLackey/gists')
+//     .then(res => {
+//       console.log(res.body[0].url);
+//       response.status(200).json(res.body[0].url);
+//     });
+// }
 
 module.exports = router;
